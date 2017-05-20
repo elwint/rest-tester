f4ed83663770545a060127d537a780c4980fd47c
Elwin Tamminga <elwintamminga@gmail.com>
2017-05-20 19:41:49 +0200

    Add test functions

--- webserver/data.py
+++ webserver/data.py
@@ -1,8 +1,12 @@
-import bcrypt
-
 import model
 import crypt
 from db import session
 
 session.add(model.User(name='Bassie', password=crypt.hash('abc')))
 session.add(model.User(name='Tester', password=crypt.hash('def')))
+session.add(model.Test(user_id=1, name='test1', last_ok=True, data='data'))
+session.add(model.Test(user_id=1, name='test2', last_ok=False, data='data'))
+session.add(model.Test(user_id=1, name='test3', last_ok=False, data='data'))
+session.add(model.Test(user_id=1, name='test4', last_ok=True, data='data'))
+session.add(model.Test(user_id=2, name='test1', last_ok=True, data='data'))
+session.add(model.Test(user_id=2, name='test2', last_ok=True, data='data'))
--- webserver/main.py
+++ webserver/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, abort
+from flask import Flask, abort, request
 import json
 
 from db import session
@@ -22,8 +22,34 @@ def page_not_found(error):
 @app.route("/users/<int:user_id>")
 def get_user_by_id(user_id):
 	user = session.query(model.User).filter_by(id=user_id).first()
-	if user == None:
+	if user is None:
 		abort(404)
 	return json.dumps({'id': user.id, 'name': user.name})
+
+# "/users/<int:user_id>" weglaten en ingelogde id gebruiken
+@app.route("/users/<int:user_id>/tests/my")
+def get_user_tests(user_id):
+	tests = session.query(model.Test).filter_by(user_id=user_id).all()
+	if tests is None:
+		abort(404)
+	json_tests = []
+	for test in tests:
+		json_tests.append({'id': test.id, 'name': test.name, 'last_ok': test.last_ok, 'data': test.data})
+	return json.dumps(json_tests)
+
+@app.route("/users/<int:user_id>/tests/add", methods=['POST'])
+def add_user_test(user_id):
+	if 'name' in request.form and 'data' in request.form:
+		session.add(model.Test(user_id=-user_id, name=request.form['name'], data=request.form['data']))
+		return json.dumps({'success': True})
+	else:
+		return json.dumps({'success': False, 'error': 'Missing data'})
+
+@app.route("/tests/<int:test_id>")
+def get_test_by_id(test_id):
+	test = session.query(model.Test).get(test_id)
+	if test is None:
+		abort(404)
+	return json.dumps({'id': test.id, 'name': test.name, 'last_ok': test.last_ok, 'data': test.data})
 
 app.run()
